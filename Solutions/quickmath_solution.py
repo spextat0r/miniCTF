6689911399981734f555aa5dc3bb7bfea1ef6777
Daniel <57763139+samiam1086@users.noreply.github.com>
2024-03-28 19:46:29 -0400

    Update quickmath_solution.py

--- Solutions/quickmath_solution.py
+++ Solutions/quickmath_solution.py
@@ -3,7 +3,7 @@
 # sending the answer back after converting from a float/int to a string then to bytes. Finally once you get the flag it prints in an error as eval() cannot understand the flag.
 import socket
 client_socket = socket.socket()
-client_socket.connect(('IP', 1337))
+client_socket.connect(('127.0.0.1', 1337))
 client_socket.recv(len('Welcome to the math challenge. You need to answer my 25 math questions rounded to the second decimal place if the answer is not a whole number really quick ok\n'))
 while True:
     client_socket.send(str(round(eval(client_socket.recv(2048).decode()), 2)).encode())
