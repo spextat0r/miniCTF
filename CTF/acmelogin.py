514a7c69948dfc16cd57a875b1a487ac032dab4f
Daniel <57763139+samiam1086@users.noreply.github.com>
2024-03-28 23:25:58 -0400

    Update acmelogin.py

--- CTF/acmelogin.py
+++ CTF/acmelogin.py
@@ -30,10 +30,10 @@ logo = """
                   ▄▄▄▄▄▄▄▄▄▄▄   ▄▄▄▄▄▄▄▄▄▄▄
                 ▄▄▄▄▄▄▄▄▄▄▄▄▄   ▄▄▄▄▄▄▄▄▄▄▄▄▄
               ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄   ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
-            
+
                            A C M E
                            SYSTEMS
-                           
+
 Your Credentials are safely stored in the file 'creds_acmeloginctf'
 and are checked each time at login for changes
 
@@ -42,64 +42,6 @@ You can request the code by sending give_code as the username
 """
 
 code = """
-def handle_client(conn, addr):
-    print("\\n" + color_BLU + f"[NEW CONNECTION] {addr} connected.\\n" + color_reset)
-
-    conn.send(logo.encode())
-
-    while True:
-        try:
-
-            with open('creds_acmeloginctf', 'r') as f:
-                cred_data = f.read()
-                f.close()
-
-            cred_data = cred_data.split('\\n')
-            while '' in cred_data:
-                cred_data.remove('')
-
-            username = cred_data[0]
-            password = cred_data[1]
-
-
-            conn.send('username: '.encode())
-            given_username = conn.recv(8192).decode()
-            if given_username.endswith('\\n'):
-                given_username = given_username[:given_username.rfind('\\n')]
-
-            print('{}: Gave a username of: "{}"'.format(addr[0].replace('\\'', ''), given_username))
-
-            if eval('\\'{}\' == \\'{}\\''.format(username, given_username)):
-                conn.send('password: '.encode())
-
-                given_password = conn.recv(8192).decode()
-
-                if given_password.endswith('\\n'):
-                    given_password = given_password[:given_password.rfind('\\n')]
-
-                print('{}: Gave a password of: "{}"'.format(addr[0].replace('\\'', ''), given_password))
-
-                if eval('\\'{}\\' == \\'{}\\''.format(password, given_password)):
-                    conn.send('Successfully loggedin!\\n'.encode())
-                    conn.send('Heres your flag: noflag4uIremovedIT\\n'.encode())
-                    conn.close()
-                    return
-                else:
-                    conn.send('Invalid Password\\n'.encode())
-                    continue
-            else:
-                conn.send('Invalid Username\\n'.encode())
-                continue
-
-        except Exception as e:
-            print(str(e))
-            import traceback
-            traceback.print_exc()
-            conn.send('What did you just send me?????\\n'.encode())
-            conn.close()
-            return
-"""
-
 def handle_client(conn, addr):
     print("\n" + color_BLU + f"[NEW CONNECTION] {addr} connected.\n" + color_reset)
 
@@ -117,6 +59,65 @@ def handle_client(conn, addr):
             username = cred_data.split(':')[0]
             password = cred_data.split(':')[1]
 
+            conn.send('username: '.encode())
+            given_username = conn.recv(8192).decode()
+            if given_username.endswith('\n'):
+                given_username = given_username[:given_username.rfind('\n')]
+
+            print('{}: Gave a username of: "{}"'.format(addr[0].replace('\'', ''), given_username))
+
+            if given_username == 'give_code':
+                conn.send(code.encode())
+                continue
+
+            if eval('\'{}\' == \'{}\''.format(username, given_username)):
+                conn.send('password: '.encode())
+
+                given_password = conn.recv(8192).decode()
+
+                if given_password.endswith('\n'):
+                    given_password = given_password[:given_password.rfind('\n')]
+
+                print('{}: Gave a password of: "{}"'.format(addr[0].replace('\'', ''), given_password))
+
+                if eval('\'{}\' == \'{}\''.format(password, given_password)):
+                    conn.send('Successfully loggedin!\n'.encode())
+                    conn.send('Heres your flag: noFlag5U\n'.encode())
+                    conn.close()
+                    return
+                else:
+                    conn.send('Invalid Password\n'.encode())
+                    continue
+            else:
+                conn.send('Invalid Username\n'.encode())
+                continue
+
+        except Exception as e:
+            print(str(e))
+            import traceback
+            conn.send('What did you just send me?????\n'.encode())
+            conn.send(traceback.format_exc().encode())
+            conn.close()
+            return
+"""
+
+
+def handle_client(conn, addr):
+    print("\n" + color_BLU + f"[NEW CONNECTION] {addr} connected.\n" + color_reset)
+
+    conn.send(logo.encode())
+
+    while True:
+        try:
+
+            with open('creds_acmeloginctf', 'r') as f:
+                cred_data = f.read()
+                f.close()
+
+            cred_data = cred_data.replace('\n', '')
+
+            username = cred_data.split(':')[0]
+            password = cred_data.split(':')[1]
 
             conn.send('username: '.encode())
             given_username = conn.recv(8192).decode()
@@ -173,7 +174,7 @@ def start():
             conn.close()
 
 
-#this starts the server which calls start() to create the handle client thread
+# this starts the server which calls start() to create the handle client thread
 def server_start():
     print("[STARTING] server is starting...")
     try:
@@ -183,14 +184,16 @@ def server_start():
         print(color_RED + "[ERROR] " + str(e) + color_reset)
     time.sleep(1)
 
+
 def main():
     yn = input('WARNING: This service will render your host vulnerable as long as the ctf script is running.\nYou are solely liable for any damage that occurs by running this tool.\nONLY run it on a secure network or localhost.\nDo you want to proceed y/n: ')
     if yn.lower() == 'y':
-        with open('creds_acmeloginctf', 'w') as f: # make the creds file
+        with open('creds_acmeloginctf', 'w') as f:  # make the creds file
             our_uname = ''.join(random.choices(string.ascii_uppercase, k=random.randrange(8, 15)))
             our_pass = ''.join(random.choices(string.ascii_uppercase + string.ascii_lowercase, k=random.randrange(18, 25)))
             f.write('{}:{}'.format(our_uname, our_pass))
         server_start()
+
 
 if __name__ == '__main__':
     main()
